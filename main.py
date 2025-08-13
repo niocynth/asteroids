6cc15e2f8ed3ecfaef30890cf2b5315e1a29dcb1
niocynth <dev@niocynth.io>
2025-08-13 17:13:45 +0100

    optimised shots

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def main():
                 print("Game over!")
                 sys.exit()
 
-        for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision(shot):
                     asteroid.kill()
