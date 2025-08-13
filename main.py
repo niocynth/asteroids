daef3878239ab4c672985f24878355d656a84e66
niocynth <dev@niocynth.io>
2025-08-13 17:11:37 +0100

    C3L5 - shots destroy asteroids

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
+
         screen.fill("black")
 
         for obj in drawable:
