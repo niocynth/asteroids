25e8ce9f3fdebcf142ac3b7d21a018c74584696c
niocynth <dev@niocynth.io>
2025-08-13 11:07:19 +0100

    C2L5 - Draw player

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
 from constants import *
-
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit framerate to 60 FPS
