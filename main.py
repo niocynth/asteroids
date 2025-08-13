0e684864cee1153636239cf8a6a41b5642886f71
niocynth <dev@niocynth.io>
2025-08-13 11:50:56 +0100

    C2L6 - Rotate Player

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main():
                 return
         screen.fill(color="black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         # limit framerate to 60 FPS
