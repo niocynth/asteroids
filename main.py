5e9682f25f6311f7e7ffdaf1880c1039a8f4ee07
niocynth <dev@niocynth.io>
2025-08-13 10:50:29 +0100

    C2L3 - Limit FPS to 60

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+        screen.fill(color="black")
         pygame.display.flip()
 
-
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
