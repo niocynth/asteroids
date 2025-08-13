05aeacb08da8bbc0895e013311e30a15742a3ab4
niocynth <dev@niocynth.io>
2025-08-13 12:29:57 +0100

    C2L8 - Moved Player to sprite groups

--- main.py
+++ main.py
@@ -7,15 +7,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
-        player.draw(screen)
-        player.update(dt)
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 FPS
