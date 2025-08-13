aa1a4914712ebde7e67996fc266b9d647cc824a8
niocynth <dev@niocynth.io>
2025-08-13 17:26:31 +0100

    C3L6 - split large asteroids

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
